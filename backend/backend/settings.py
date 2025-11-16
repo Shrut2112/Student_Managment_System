04d9f717b32ba61a72e90b59c9fd0efca0ff7986
Shrut2112 <jainshrutd211204@gmail.com>
2025-11-16 12:28:11 +0530

    added db info and connected with uvicorn

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_amx_age = 600
+        conn_max_age = 600
     )
     # {
     #     "ENGINE": "django.db.backends.postgresql",
