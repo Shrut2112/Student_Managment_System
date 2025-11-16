f3bda84fbed91cbff2531fdcf1ecea1bc8ee36e4
Shrut2112 <jainshrutd211204@gmail.com>
2025-11-16 14:30:17 +0530

    updated setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,13 +73,13 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://student-managment-system-beta.vercel.app/"
+    "https://student-managment-system-beta.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://student-managment-system-beta.vercel.app/"
+    "https://student-managment-system-beta.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
