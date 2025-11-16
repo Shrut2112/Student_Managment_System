bfb0f21b30bb5d4efba9d74567c6429924c7acbc
Shrut2112 <jainshrutd211204@gmail.com>
2025-11-16 14:40:57 +0530

    updated setting.py

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
     
     try{
 
-      const response = await axios.post('http://localhost:8000/login', formData);
+      const response = await axios.post('https://student-managment-system-beta.vercel.app/login', formData);
       console.log("Server Response:", response.data);
 
       localStorage.setItem('access_token',response.data.access);
