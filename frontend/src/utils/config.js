b6b3200387db54ea9b579e442213a0e2d6f36c6f
Shrut2112 <jainshrutd211204@gmail.com>
2025-11-16 13:45:40 +0530

    Connected frontend-backend

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = "https://student-managment-system-beta.vercel.app/";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
